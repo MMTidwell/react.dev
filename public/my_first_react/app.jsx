33e24cd31fed2766a8e68dc81ae81650be2533d6
Mittsy Tidwell <mtidwell506@gmail.com>
2017-01-02 10:35:05 -0600

    trying to get react enviroment to work

---
+++ public/my_first_react/app.jsx
@@ -0,0 +1,3 @@
+// ReactDOM.render - renders code to page
+	// takes 2 arguments - virtual DOM element, real DOM element where we would like to place the virtual DOM element
+ReactDOM.render(<h1>Hello</h1>, document.getElementById("container"));
