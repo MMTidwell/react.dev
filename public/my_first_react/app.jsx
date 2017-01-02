362363da8041655dddc38f4a6c32bab51936c126
Mittsy Tidwell <mtidwell506@gmail.com>
2017-01-02 11:52:11 -0600

    finished my first react dot

--- public/my_first_react/app.jsx
+++ public/my_first_react/app.jsx
@@ -1,3 +1,19 @@
+// Componates in REACT sould start with a Cap letter
+function Application () {
+	return (
+		<div>
+			<h1>Hello from REACT!</h1>
+			<p>I was rendered fromthe Application component!</p>
+		</div>
+	);
+}
+
+
+// This should place "Hello" where "Loading..." is in the HTML file
+// ---------
 // ReactDOM.render - renders code to page
-	// takes 2 arguments - virtual DOM element, real DOM element where we would like to place the virtual DOM element
-ReactDOM.render(<h1>Hello</h1>, document.getElementById("container"));
+	// takes 2 arguments - (virtual DOM element, real DOM element where we would like to place the virtual DOM element)
+// ReactDOM.render(<h1>Hello</h1>, document.getElementById("container"));
+
+// In order to call the Application Component function, call that in place of the first argument. Note that since it does not have any child elements it does not need to have a closing tag and we will simply put "/>" at the end of the call
+ReactDOM.render(<Application />, document.getElementById('container'));
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
-Learing REACT!
+Learning REACT!!
+
+I am learning REACT via TreeHouse and this repo is from the course. When I first started learning REACT I tried to build my enviroment and found that it was not as easy as it looked. So I decided to build everything in my current enviorment, and test it in theirs.
+
+You can see the programs that I built in TreeHouse if you look in the public folder. Enjoy! :)
