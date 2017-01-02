0a8b4ae580f55827fb221138a55a071d4b680975
Mittsy Tidwell <mtidwell506@gmail.com>
2017-01-02 14:35:19 -0600

    using component to make it easier to read

--- public/scoreboard/score.jsx
+++ public/scoreboard/score.jsx
@@ -1,68 +1,65 @@
+function Header(props) {
+	return (
+		<div className="header">
+			<h1> {props.title} </h1>
+		</div>
+	); 
+};
+
+Header.propType = {
+	title: React.PropTypes.string.isRequired,
+};
+
+function Counter(props) {
+	return (
+		<div className="counter">
+			<button className="counter-action decrement"> 
+				- 
+			</button>
+			<div className="counter-score">
+				{props.score}
+			</div>
+			<button className="counter-action increment">
+				+
+			</button>
+		</div>
+	);
+};
+
+Counter.propType = {
+	score: React.PropTypes.number.isRequired,
+};
+
+function Player(props) {
+	return (
+		<div className="player">
+			<div className="player-name">
+				{props.name}
+			</div>
+			<div className="player-score">
+				<Counter score={props.score} />
+			</div>
+		</div>
+	);
+};
+
+Player.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	score: React.PropTypes.number.isRequired,
+};
+
+
 // {props.title} - will call what is in the Application arg. Then the arg is pulled from the called section in the REACT line bellow. {} must be used since plan JS is being used here
+// <Header title = {props.title} /> - This calls the Header function and passes the props.title
 function Application (props) {
 	return (
 		<div className="scoreboard">
-			<div className="header">
-				<h1>{props.title}</h1>
-			</div>
+			<Header title = {props.title} />
 
 			<div className="players">
-				<div className="player">
-					<div className="player-name">
-						Mitt Tidwell
-					</div>
-					<div className="player-score">
-						<div className="counter">
-							<button className="counter-action decrement"> 
-								- 
-							</button>
-							<div className="counter-score">
-								32
-							</div>
-							<button className="counter-action increment">
-								+
-							</button>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						Tim Kessler
-					</div>
-					<div className="player-score">
-						<div className="counter">
-							<button className="counter-action decrement">
-								-
-							</button>
-							<div className="counter-score">
-								29
-							</div>
-							<button className="counter-action increment">
-								+
-							</button>
-						</div>
-					</div>
-				</div>
-
-				<div className="player">
-					<div className="player-name">
-						Marley Dozer
-					</div>
-					<div className="player-score">
-						<div className="counter">
-							<button className="counter-action decrement">
-								-
-							</button>
-							<div className="counter-score">
-								8
-							</div>
-							<button className="counter-action increment">
-							 	+
-							 </button>
-						</div>
-					</div>
-				</div>
+				<Player name="Mitt Tidwell" score={32} />
+				<Player name="Tim Kessler" score={29} />
+				<Player name="Marley Dozer" score={8} />
 			</div>
 		</div>
 	);
